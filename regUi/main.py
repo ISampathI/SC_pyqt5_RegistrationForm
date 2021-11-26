27a3f95c38148b535f1312dd0cacf3346fc6bc38
SihinaCode <89958555+SihinaCode@users.noreply.github.com>
2021-11-26 14:15:49 +0530

    Update main.py

--- regUi/main.py
+++ regUi/main.py
@@ -12,6 +12,12 @@ class RegApp(QtWidgets.QWidget, Ui_Form):
         self.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        
+        self.label.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=0, yOffset=0))
+        self.label_3.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=0, yOffset=0))
+        self.pushButton.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=3, yOffset=3))
+        self.pushButton_6.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=3, yOffset=3))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
